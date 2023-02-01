ccf2127f2d7cf1e37869490c59895b8c183de2be
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2023-02-01 00:01:55 -0600

    Add constant for session_secret

--- hangman.rb
+++ hangman.rb
@@ -2,6 +2,8 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require "tilt/erubis"
 
+SECRET = SecureRandom.hex(32)
+
 configure do
   enable :sessions
   set :session_secret, SECRET
@@ -12,5 +14,33 @@ end
 # choice when letters are clicked I can hide them with css
 
 get '/' do
+  erb :welcome
+end
+
+get '/empty_gallows' do
   erb :empty_gallows
+end
+
+get '/head_only' do
+  erb :head_only
+end
+
+get '/head_and_body' do
+  erb :head_and_body
+end
+
+get '/one_limb' do
+  erb :one_limb
+end
+
+get '/two_limbs' do
+  erb :two_limbs
+end
+
+get '/three_limbs' do
+  erb :three_limbs
+end
+
+get '/full_body' do
+  erb :full_body
 end
